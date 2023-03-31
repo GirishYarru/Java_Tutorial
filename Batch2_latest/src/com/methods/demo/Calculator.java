b0c32320a92f6a74a7df43bf99ed9d28670bcbb5
GirishYarru <venkatgirish5@gmail.com>
2023-03-31 10:45:16 +0530

    batch1-methods demo, batch2 - jdbc demo

---
+++ Batch2_latest/src/com/methods/demo/Calculator.java
@@ -0,0 +1,43 @@
+package com.methods.demo;
+
+public class Calculator {
+	
+	int temp = 10;
+	static int index= 10;
+
+	public int add(int a, int b) {
+		return a + b;
+	}
+
+	public int sub(int a, int b) {
+		return a - b;
+	}
+
+	public int multiply(int a, int b) {
+		return a * b;
+	}
+
+	public String welcome() {
+		return "Welcome";
+	}
+	
+	public void hai() {
+		
+		String str = "good moring";
+		
+		System.out.println("helloooo"+str);
+		
+		
+		
+	}
+
+	public static void main(String[] args) {
+		System.out.println(" Hi welcome!");
+		Calculator cal = new Calculator();
+		System.out.println(" 1st operation - multiplication");
+	//	int c = cal.multiply(3, 5);
+		System.out.println(cal.add(3, 5));
+		cal.hai();
+	}
+
+}
---
+++ Batch2_latest/src/com/methods/demo/MethodsDemo.java
@@ -0,0 +1,11 @@
+package com.methods.demo;
+
+public class MethodsDemo {
+	
+	public static void main(String[] args) {
+		Calculator cal = new Calculator();
+		cal.add(5, 5);
+		
+	}
+
+}
